744a44d0a4b6348531a72e7ccc318820be29d563
danial_derayati <dani903870@gmail.com>
2022-03-11 21:23:33 +0330

    add calculation and result

--- clac_py.py
+++ clac_py.py
@@ -1,8 +1,19 @@
+from decimal import DivisionByZero
+from attr import validate
+from idna import valid_contextj
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 class Cgrid(GridLayout):
-    pass
+    def cal(self,result):
+        if result:
+            try:
+                self.show.text=str(eval(result))
+            except:
+                self.show.text = "ERROR"
+
+
+        
 
 class calculatorApp(App):
     def build(self):
