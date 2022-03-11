96740af62aa7dbf40a8dfd1bcaea852d743f31ab
danial_derayati <dani903870@gmail.com>
2022-03-11 18:53:26 +0330

    add kv gridlayout

--- clac_py.py
+++ clac_py.py
@@ -1,2 +1,13 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+
+class Cgrid(GridLayout):
+
+    pass
+
+class calculatorApp(App):
+    def build(self):
+        return Cgrid()
+
+
+calculatorApp().run()
